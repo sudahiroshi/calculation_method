ba1adde8446bde14b28d7c43489cad5c1970ef49
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2025-08-19 15:48:18 +0900

    juliaを追加

---
+++ julia/1-1.jl
@@ -0,0 +1,32 @@
+using Printf
+
+const EPS = 0.0001
+
+function nibun( a, b )
+    c = 0
+    while true
+        c = ( a + b ) / 2.0
+        if ( func_y(c) * func_y(a) ) < 0
+            b = c
+        else
+            a = c
+        end
+        if !( abs( a - b) > EPS )
+            break
+        end
+    end
+    return c
+end
+
+function func_y( x )
+    return x^3.0 + x - 1.0
+end
+
+a = 0.0
+b = 1.0
+
+println( "x^3 + x - 1 = 0 の2分法による数値計算\n" )
+@printf( "初期値a = %6.3lf\n", a )
+@printf( "初期値b = %6.3lf\n", b )
+x = nibun( a, b )
+@printf( "近似解x = %6.3lf\n", x )
